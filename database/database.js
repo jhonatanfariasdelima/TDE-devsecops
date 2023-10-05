4ab44d09fa3aa36cb8dab8408085c770b3ab9b64
root <root@ubuntu-2204.linuxvmimages.local>
2023-10-05 13:14:19 -0400

    87654

--- database/database.js
+++ database/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 
 const password = process.env.MYSQL_PASSWORD;
 
-const connection = new Sequelize('banco','root', password,{
+const connection = new Sequelize('banco','root', 'root',{
     host: '172.19.0.2',
     dialect: 'mysql'
 });
