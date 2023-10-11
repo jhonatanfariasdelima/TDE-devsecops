bfc99794d0508a8a1dd132fd3b5580ceb5e83987
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-10-11 09:39:50 -0400

    349

--- test/index.test.js
+++ test/index.test.js
@@ -3,16 +3,6 @@ let supertest = require("supertest");
 let request = supertest(app);
 
 
-test("A aplicação deveria responder",() => {
-
-    return request.get("/").then(res => {
-        let status = res.statusCode
-        expect(status).toEqual(200);
-    }).catch(err => {
-        fail(err)
-    });
-})
-
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
