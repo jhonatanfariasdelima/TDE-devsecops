6dfcd8d762c3517e12f502fe13ba16497b948f02
root <root@ubuntu-2204.linuxvmimages.local>
2023-11-22 19:00:09 -0500

    kubernetes

--- index.js
+++ index.js
@@ -2,24 +2,12 @@ const express = require("express");
 const session = require("express-session");
 const app = express();
 const bodyParser = require("body-parser");
-//const connection = require("./database/database");
-//const loginModel = require("./database/Login");
+
 
 //session
 app.use(session({
     secret: "asd", cookie: {maxAge: 300000}
 }));
-
-
-// //Database
-// connection
-//     .authenticate()
-//     .then(() => {
-//         //console.log("Conexão feita com o banco de dados!")
-//     })
-//     .catch((msgErro) => {
-//         console.log(msgErro);
-//     })
 
 
 app.set("view engine", "ejs");
@@ -34,38 +22,6 @@ app.get("/",(req, res) => {
     res.render("index");
 });
 
-// app.post("/login",(req, res) => {
-//     var user = req.body.username;
-//     var password = req.body.password;
-
-
-//     // loginModel.findOne({
-//     //     where: {user: user}
-//     // }).then(login => {
-
-//         if(user == "admin"){
-//             if(password == "admin"){
-//                 req.session.user = {
-//                     user: user
-//                 }
-//                 res.redirect("/home");
-//             }else{
-                
-//                 res.send("Login ou senha não encontrado");
-//             }
-//         }else{
-            
-//             res.status(403).send("Login ou senha não encontrado");
-//         }
-//     // });
-// });
-
-// app.get("/home",(req, res) => {
-//     var nome = req.session.user.user;
-//     res.render("home",{
-//         nome: nome
-//     });
-// })
 
 module.exports = app;
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,45 +1,16 @@
-let app = require("../index");
-let supertest = require("supertest");
-let request = supertest(app);
+// let app = require("../index");
+// let supertest = require("supertest");
+// let request = supertest(app);
 
 
-test("A aplicação deveria responder",() => {
+// test("A aplicação deveria responder",() => {
 
-    return request.get("/").then(res => {
-        let status = res.statusCode
-        expect(status).toEqual(200);
-    }).catch(err => {
-        fail(err)
-    });
-})
-
-// function sleep(ms) {
-//     return new Promise(resolve => setTimeout(resolve, ms));
-// }
-
-// test("Teste no login de usuario cadastrado",async () => {
-//     await sleep(2000);
-//     let user = {username: "admin", password: "123"};
-
-//     return request.post("/login").send(user).then(res => {
+//     return request.get("/").then(res => {
 //         let status = res.statusCode
-//         expect(status).toEqual(302);
+//         expect(status).toEqual(200);
 //     }).catch(err => {
-//         console.log(err);
 //         fail(err)
 //     });
-// })
+// // })
 
-// test("Teste no login de usuario NÃO cadastrado",async () => {
-//     await sleep(2000);
-//     let user = {username: "zé da manga", password: "123"};
-
-//     return request.post("/login").send(user).then(res => {
-//         let status = res.statusCode
-//         expect(status).toEqual(403);
-//     }).catch(err => {
-//         console.log(err);
-//         fail(err)
-//     });
-// })
 
